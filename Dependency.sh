e035d9317c3e1ab73256e391614968122cf485fa
Keyur Bhatt <keyur.bhatt.serpentcs@gmail.com>
2016-02-22 19:03:15 +0530

    [Edited Dependency.sh file]

--- Dependency.sh
+++ Dependency.sh
@@ -3,40 +3,40 @@
 
 sudo apt-get install -y python-dateutil python-lxml python-polib python-relatorio python-ldap python-vobject python-stdnum build-essential python-dev python-pip libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev python-ldap bsdtar python-imaging python2.7-cracklib postgresql postgresql-server-dev-all libjpeg-dev &&
 
-sudo pip install simpleeval &&
+sudo pip install simpleeval --upgrade &&
 
-sudo pip install hl7apy &&
+sudo pip install hl7apy --upgrade &&
 
-sudo pip install pytz &&
+sudo pip install pytz --upgrade &&
 
-sudo pip install pywebdav && 
+sudo pip install pywebdav --upgrade && 
 
-sudo pip install lxml &&
+sudo pip install lxml --upgrade &&
 
-sudo pip install relatorio &&
+sudo pip install relatorio --upgrade &&
 
-sudo pip install python-dateutil &&
+sudo pip install python-dateutil --upgrade &&
 
-sudo pip install psycopg2 &&
+sudo pip install psycopg2 --upgrade &&
 
-sudo pip install pytz &&
+sudo pip install pytz --upgrade &&
 
-sudo pip install python-ldap &&
+sudo pip install python-ldap --upgrade &&
 
-sudo pip install vobject &&
+sudo pip install vobject --upgrade &&
 
-sudo pip install PyWebDAV &&
+sudo pip install PyWebDAV --upgrade &&
 
-sudo pip install qrcode &&
+sudo pip install qrcode --upgrade &&
 
-sudo pip install six &&
+sudo pip install six --upgrade &&
 
-sudo pip install caldav &&
+sudo pip install caldav --upgrade &&
 
-sudo pip install polib &&
+sudo pip install polib --upgrade &&
 
-sudo pip install python-sql &&
+sudo pip install python-sql --upgrade &&
 
-sudo pip install python-stdnum &&
+sudo pip install python-stdnum --upgrade &&
 
-sudo pip install simpleeval
+sudo pip install simpleeval --upgrade
