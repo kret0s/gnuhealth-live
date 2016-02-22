3e8adee46a9300e805c2f311f07c45561f8095aa
Keyur Bhatt <keyur.bhatt.serpentcs@gmail.com>
2016-02-22 19:00:01 +0530

    [Edited Dependency.sh file]

--- Dependency.sh
+++ Dependency.sh
@@ -1,7 +1,7 @@
 # Install Dependencies using the command 'sudo sh Dependeny.sh' 
 
 
-sudo apt-get install python-dateutil python-lxml python-polib python-relatorio python-ldap python-vobject python-stdnum build-essential python-dev python-pip libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev python-ldap bsdtar python-imaging python2.7-cracklib postgresql postgresql-server-dev-all libjpeg-dev &&
+sudo apt-get install -y python-dateutil python-lxml python-polib python-relatorio python-ldap python-vobject python-stdnum build-essential python-dev python-pip libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev python-ldap bsdtar python-imaging python2.7-cracklib postgresql postgresql-server-dev-all libjpeg-dev &&
 
 sudo pip install simpleeval &&
 
@@ -9,4 +9,34 @@ sudo pip install hl7apy &&
 
 sudo pip install pytz &&
 
-sudo pip install pywebdav
+sudo pip install pywebdav && 
+
+sudo pip install lxml &&
+
+sudo pip install relatorio &&
+
+sudo pip install python-dateutil &&
+
+sudo pip install psycopg2 &&
+
+sudo pip install pytz &&
+
+sudo pip install python-ldap &&
+
+sudo pip install vobject &&
+
+sudo pip install PyWebDAV &&
+
+sudo pip install qrcode &&
+
+sudo pip install six &&
+
+sudo pip install caldav &&
+
+sudo pip install polib &&
+
+sudo pip install python-sql &&
+
+sudo pip install python-stdnum &&
+
+sudo pip install simpleeval
